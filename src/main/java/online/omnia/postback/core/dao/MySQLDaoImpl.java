811162bc2623ffcaa4c1d10200bfdf7461741b48
Vladislav <vladislav.krets@gmail.com>
2017-08-07 18:32:37 +0300

    fixed checks with sessions

--- src/main/java/online/omnia/postback/core/dao/MySQLDaoImpl.java
+++ src/main/java/online/omnia/postback/core/dao/MySQLDaoImpl.java
@@ -5,6 +5,7 @@ import online.omnia.postback.core.trackers.entities.PostBackEntity;
 import online.omnia.postback.core.trackers.entities.AdvertsEntity;
 import online.omnia.postback.core.trackers.entities.TrackerEntity;
 import online.omnia.postback.core.utils.FileWorkingUtils;
+import org.hibernate.HibernateException;
 import org.hibernate.NonUniqueResultException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -57,64 +58,152 @@ public class MySQLDaoImpl implements MySQLDao {
 
 
     public PostBackEntity getPostbackByTransactionId(String transactionId) {
-        Session session = sessionFactory.openSession();
+        Session session = null;
         PostBackEntity postBackEntity = null;
-        try {
-            postBackEntity = session.createQuery("from PostBackEntity where transactionid=:transactionid", PostBackEntity.class)
-                    .setParameter("transactionid", transactionId).getSingleResult();
-        } catch (NoResultException e) {
-            postBackEntity = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                postBackEntity = null;
+                try {
+                    postBackEntity = session.createQuery("from PostBackEntity where transactionid=:transactionid", PostBackEntity.class)
+                            .setParameter("transactionid", transactionId).getSingleResult();
+                } catch (NoResultException e) {
+                    postBackEntity = null;
+                }
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
         session.close();
         return postBackEntity;
     }
+
     @Override
     public void addPostback(PostBackEntity postBackEntity) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        if (postBackEntity.getTransactionId() == null) postBackEntity.setTransactionId(postBackEntity.getActionId());
-        session.save(postBackEntity);
-        session.getTransaction().commit();
+        Session session = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                session.beginTransaction();
+                if (postBackEntity.getTransactionId() == null)
+                    postBackEntity.setTransactionId(postBackEntity.getActionId());
+                session.save(postBackEntity);
+                session.getTransaction().commit();
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
         session.close();
     }
 
     @Override
     public List<AdvertsEntity> getAllAdverts() {
-        Session session = sessionFactory.openSession();
+
+        Session session = null;
         List<AdvertsEntity> advertsEntityList
-                = session.createQuery("from Adverts", AdvertsEntity.class).getResultList();
+                = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                advertsEntityList = session.createQuery("from Adverts", AdvertsEntity.class).getResultList();
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
         session.close();
         return advertsEntityList;
     }
 
     @Override
     public List<TrackerEntity> getTrackers() {
-        Session session = sessionFactory.openSession();
-        List<TrackerEntity> trackerEntityList =
-                session.createQuery("from TrackerEntity", TrackerEntity.class).getResultList();
+        Session session = null;
+        List<TrackerEntity> trackerEntityList = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                trackerEntityList = session.createQuery("from TrackerEntity", TrackerEntity.class).getResultList();
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
         session.close();
         return trackerEntityList;
     }
 
     @Override
     public List<AffiliatesEntity> getAffiliates() {
-        Session session = sessionFactory.openSession();
-        List<AffiliatesEntity> affiliates =
-                session.createQuery("from AffiliatesEntity", AffiliatesEntity.class).getResultList();
+        Session session = null;
+        List<AffiliatesEntity> affiliates = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                affiliates = session.createQuery("from AffiliatesEntity", AffiliatesEntity.class).getResultList();
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
         session.close();
         return affiliates;
     }
 
     @Override
     public AffiliatesEntity getAffiliateByAffid(int afid) {
-        AffiliatesEntity affiliate;
-        Session session = sessionFactory.openSession();
-        try {
-            affiliate = session.createQuery("from AffiliatesEntity where afid=:afid",
-                    AffiliatesEntity.class).setParameter("afid", afid).getSingleResult();
-        }
-        catch (NoResultException e) {
-            affiliate = null;
+        AffiliatesEntity affiliate = null;
+        Session session = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                try {
+                    affiliate = session.createQuery("from AffiliatesEntity where afid=:afid",
+                            AffiliatesEntity.class).setParameter("afid", afid).getSingleResult();
+                } catch (NoResultException e) {
+                    affiliate = null;
+                }
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
         session.close();
         return affiliate;
@@ -123,24 +212,36 @@ public class MySQLDaoImpl implements MySQLDao {
     @Override
     public PostBackEntity getPostbackByClickAndTransactionId(String clickId, String transactionId) {
 
-        Session session = sessionFactory.openSession();
-        PostBackEntity postBackEntity;
-        try {
-            postBackEntity = session.createQuery("from PostBackEntity where clickid=:clickId and transactionid=:transactionId",
-                    PostBackEntity.class)
-                    .setParameter("clickId", clickId)
-                    .setParameter("transactionId", transactionId)
-                    .getSingleResult();
-        }
-        catch (NoResultException e) {
-            postBackEntity = null;
-        }
-        catch (NonUniqueResultException e) {
-            postBackEntity = session.createQuery("from PostBackEntity where clickid=:clickId and transactionid=:transactionId",
-                    PostBackEntity.class)
-                    .setParameter("clickId", clickId)
-                    .setParameter("transactionId", transactionId)
-                    .getResultList().get(0);
+        Session session = null;
+        PostBackEntity postBackEntity = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                try {
+                    postBackEntity = session.createQuery("from PostBackEntity where clickid=:clickId and transactionid=:transactionId",
+                            PostBackEntity.class)
+                            .setParameter("clickId", clickId)
+                            .setParameter("transactionId", transactionId)
+                            .getSingleResult();
+                } catch (NoResultException e) {
+                    postBackEntity = null;
+                } catch (NonUniqueResultException e) {
+                    postBackEntity = session.createQuery("from PostBackEntity where clickid=:clickId and transactionid=:transactionId",
+                            PostBackEntity.class)
+                            .setParameter("clickId", clickId)
+                            .setParameter("transactionId", transactionId)
+                            .getResultList().get(0);
+                }
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
         session.close();
         return postBackEntity;
@@ -148,22 +249,35 @@ public class MySQLDaoImpl implements MySQLDao {
 
     @Override
     public PostBackEntity getPostbackByFullUrl(String fullUrl) {
-        Session session = sessionFactory.openSession();
-        PostBackEntity postBackEntity;
-        try {
-            postBackEntity = session.createQuery("from PostBackEntity where fullurl=:fullurl",
-                    PostBackEntity.class)
-                    .setParameter("fullurl", fullUrl)
-                    .getSingleResult();
-        }
-        catch (NoResultException e) {
-            postBackEntity = null;
-        }
-        catch (NonUniqueResultException e) {
-            postBackEntity = session.createQuery("from PostBackEntity where fullurl=:fullurl",
-                    PostBackEntity.class)
-                    .setParameter("fullurl", fullUrl)
-                    .getResultList().get(0);
+        Session session = null;
+        PostBackEntity postBackEntity = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                try {
+                    postBackEntity = session.createQuery("from PostBackEntity where fullurl=:fullurl",
+                            PostBackEntity.class)
+                            .setParameter("fullurl", fullUrl)
+                            .getSingleResult();
+                } catch (NoResultException e) {
+                    postBackEntity = null;
+                } catch (NonUniqueResultException e) {
+                    postBackEntity = session.createQuery("from PostBackEntity where fullurl=:fullurl",
+                            PostBackEntity.class)
+                            .setParameter("fullurl", fullUrl)
+                            .getResultList().get(0);
+                }
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
         }
         session.close();
         return postBackEntity;
@@ -171,14 +285,28 @@ public class MySQLDaoImpl implements MySQLDao {
 
     @Override
     public TrackerEntity getTrackerByPrefix(String prefix) {
-        Session session = sessionFactory.openSession();
-        TrackerEntity trackerEntity;
-        try {
-            trackerEntity = session.createQuery("from TrackerEntity where prefix=:prefix",
-                    TrackerEntity.class)
-                    .setParameter("prefix", prefix).getSingleResult();
-        } catch (NoResultException e) {
-            trackerEntity = null;
+        Session session = null;
+        TrackerEntity trackerEntity = null;
+        while (true) {
+            try {
+                session = sessionFactory.openSession();
+                try {
+                    trackerEntity = session.createQuery("from TrackerEntity where prefix=:prefix",
+                            TrackerEntity.class)
+                            .setParameter("prefix", prefix).getSingleResult();
+                } catch (NoResultException e) {
+                    trackerEntity = null;
+                }
+                break;
+            } catch (PersistenceException e) {
+                try {
+                    System.out.println("Can't connect to db");
+                    System.out.println("Waiting for 30 seconds");
+                    Thread.sleep(30000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
         session.close();
         return trackerEntity;
